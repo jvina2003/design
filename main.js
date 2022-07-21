4cf918eefe744fd3eef8e67f1c55a1d87a97462d
jvina2003 <jvina2003@outlook.com>
2022-07-21 23:05:12 +0700

    hi

--- main.js
+++ main.js
@@ -40,17 +40,47 @@ function toggleDarkLight() {
 
 var mybutton = document.getElementById("myBtn");
 
-window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
 
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+//this is where we apply opacity to the arrow
+$(window).scroll( function(){
+
+	//get scroll position
+	var topWindow = $(window).scrollTop();
+	//multipl by 1.5 so the arrow will become transparent half-way up the page
+	var topWindow = topWindow * 1.5;
+	
+	//get height of window
+	var windowHeight = $(window).height();
+		
+	//set position as percentage of how far the user has scrolled 
+	var position = topWindow / windowHeight;
+	//invert the percentage
+	position = 1 - position;
+  
+	//define arrow opacity as based on how far up the page the user has scrolled
+	//no scrolling = 1, half-way up the page = 0
+	$('.arrow-wrap').css('opacity', position);
+  
+  });
+  
+  
+  
+  
+  
+  
+  //Code stolen from css-tricks for smooth scrolling:
+  $(function() {
+	$('a[href*=#]:not([href=#])').click(function() {
+	  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		var target = $(this.hash);
+		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		if (target.length) {
+		  $('html,body').animate({
+			scrollTop: target.offset().top
+		  }, 1000);
+		  return false;
+		}
+	  }
+	});
+  });
